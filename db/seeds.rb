91b9d5e0941c7d53652c1ce98f06a49f1449da25
AnneLaure Le <annelaurele@hotmail.fr>
2022-02-17 20:18:24 +0100

    models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'Date'
+
+#Creation users
+
+al = User.create(email: "annelaurele@hotmail.fr", password: "12345678", name: "Anne-Laure", last_name: "Le", address: "Toulouse", mobile: "0617373935", status: "client")
+julia = User.create(email: "julia814@hotmail.fr", password: "12345678", name: "Julia", last_name: "Foucher", address: "Paris", mobile: "0617373935", status: "owner")
+
+#Creation shop
+
+shop = Shop.create(name: "Boulangerie", address: "Paris", description: "Ceci est une description", mobile: "0617373935", email: "boulangerie@gmail.com", category: "boulangerie")
+
+#Creation rental
+
+loc = Rental.new(rental_time_start: Time.now - 1.hours, rental_time_end: Time.now + 1.hours, status: "en cours")
+
+loc.user = julia
+loc.shop = shop
+shop.user = al
+loc.save
+al.save
