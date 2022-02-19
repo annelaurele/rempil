474a83d5b80173497ab8c74cfa5cd625fa640525
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-02-19 12:02:46 +0100

    show shop tmp

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'redis', '~> 4.0'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
+gem 'bootstrap', '~> 5.1.3'
+gem 'bundler', '~> 2.3'
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :shops #As a client
-  has_many :rentals
-  has_many :rates
+  has_many :shops, dependent: :destroy #As a client
+  has_many :rentals, dependent: :destroy
+  has_many :ratings, dependent: :destroy
   has_many :shops_as_owner, through: :shops, source: :rentals #Alias pour proprietaire/status
 end
