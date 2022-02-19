a61a355341576b77bb83af95caf9ad6ff3f5879d
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-02-19 12:25:11 +0100

    show shop fix

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,6 @@
 class ShopsController < ApplicationController
     def show
         @shop = Shop.find(params[:id])
-        @rentals_count = Rental.where(user: current_user)  #.where id = current_user ?
+        @rentals_count = Rental.where(user: current_user)  #.where id = current_user ??
     end
 end
