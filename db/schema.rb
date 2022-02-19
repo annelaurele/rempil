3348f4c5c82fcf30e90cc3ad915f066b21f37c82
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-02-19 17:42:17 +0100

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_114626) do
+ActiveRecord::Schema.define(version: 2022_02_19_153401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_114626) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "closing_hours"
     t.index ["user_id"], name: "index_shops_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ julia = User.create(name: "Julia", last_name: "Foucher", email: "julia814@hotmai
 
 marina = User.create(name: "Marina", last_name: "Magnoli", email: "mgnl.marina@gmail.com", password: "12345678", address: "16 place Etienne Esquirol 31000 Toulouse", mobile: "0617373935", status: "Grand arbre", shop: "false", image_url: ActionController::Base.helpers.image_path('marina.jpg'))
 
-elliot = User.create(name: "Elliot", last_name: "Thyrant", email: "xaron.thyrant@gmail.com", password: "12345678", address: "16 place Etienne Esquirol 31000 Toulouse", mobile: "0617373935", status: "Petite pousse", shop: "false")
+elliot = User.create(name: "Elliot", last_name: "Thyrant", email: "xaron.thyrant@gmail.com", password: "12345678", address: "16 place Etienne Esquirol 31000 Toulouse", mobile: "0617373935", status: "Petite pousse", shop: "false", image_url: ActionController::Base.helpers.image_path('arbre.jpg'))
 
 annelaure = User.create(name: "Anne-Laure", last_name: "Le", email: "annelaurele@hotmail.fr", password: "12345678", address: "16 place Etienne Esquirol 31000 Toulouse", mobile: "0617373935", shop: "true")
 
@@ -35,29 +35,29 @@ annelaure = User.create(name: "Anne-Laure", last_name: "Le", email: "annelaurele
 
 puts '----- Creating shops -----'
 
-stcyp = Shop.create(name: "Le St Cyp Traiteur", address: "1 Place de la Bourse, 31000 Toulouse", mobile: "0567687719", email: "", category: "Traiteur", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.8)
+stcyp = Shop.create(name: "Le St Cyp Traiteur", address: "1 Place de la Bourse, 31000 Toulouse", mobile: "0567687719", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.8, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-primalova = Shop.create(name: "Prima Lova", address: "1 Place de la Bourse, 31000 Toulouse", mobile: "0561447278", email: "", category: "Épicerie", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.5)
+primalova = Shop.create(name: "Prima Lova", address: "1 Place de la Bourse, 31000 Toulouse", mobile: "0561447278", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-chapristea = Shop.create(name: "Chapristea", address: "4 Rue Jules Chalande, 31000 Toulouse", mobile: "0983584945", email: "", category: "Salon de thé", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5)
+chapristea = Shop.create(name: "Chapristea", address: "4 Rue Jules Chalande, 31000 Toulouse", mobile: "0983584945", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-prosciutteria = Shop.create(name: "Prosciutteria Toulouse", address: "31 Rue des Filatiers, 31000 Toulouse", mobile: "0971216499", email: "", category: "Restaurant", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.5)
+prosciutteria = Shop.create(name: "Prosciutteria Toulouse", address: "31 Rue des Filatiers, 31000 Toulouse", mobile: "0971216499", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-flowers = Shop.create(name: "Flower's Café", address: "6 Place Roger Salengro, 31000 Toulouse", mobile: "0534449366", email: "", category: "Salon de thé", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5)
+flowers = Shop.create(name: "Flower's Café", address: "6 Place Roger Salengro, 31000 Toulouse", mobile: "0534449366", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-alimentation = Shop.create(name: "L'alimentation", address: "24 Place de la Bourse, 31000 Toulouse", mobile: "0561216905", email: "", category: "Épicerie", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4)
+alimentation = Shop.create(name: "L'alimentation", address: "24 Place de la Bourse, 31000 Toulouse", mobile: "0561216905", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-fufu = Shop.create(name: "Fufu Ramen", address: "32 Rue Sainte-Ursule, 31000 Toulouse", mobile: "0562177446", email: "", category: "Restaurant", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5)
+fufu = Shop.create(name: "Fufu Ramen", address: "32 Rue Sainte-Ursule, 31000 Toulouse", mobile: "0562177446", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-boucheb = Shop.create(name: "Bouche b", address: "36 Rue des Filatiers, 31000 Toulouse", mobile: "0561216905", email: "", category: "Restaurant", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4)
+boucheb = Shop.create(name: "Bouche b", address: "36 Rue des Filatiers, 31000 Toulouse", mobile: "0561216905", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-mamie = Shop.create(name: "Chez Mamie", address: "10 Place de la Trinité, 31000 Toulouse", mobile: "0531225030", email: "", category: "Boucherie", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.3)
+mamie = Shop.create(name: "Chez Mamie", address: "10 Place de la Trinité, 31000 Toulouse", mobile: "0531225030", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.3, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-fournil = Shop.create(name: "Fournil de Pierre", address: "16 Place Etienne Esquirol, 31000 Toulouse", mobile: "0561217484", email: "", category: "Boulangerie", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.3)
+fournil = Shop.create(name: "Fournil de Pierre", address: "16 Place Etienne Esquirol, 31000 Toulouse", mobile: "0561217484", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.3, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-lamiecaline = Shop.create(name: "La Mie Câline", address: "9 Place Etienne Esquirol, 31000 Toulouse", mobile: "0561253948", email: "", category: "Boulangerie", opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.0)
+lamiecaline = Shop.create(name: "La Mie Câline", address: "9 Place Etienne Esquirol, 31000 Toulouse", mobile: "0561253948", email: "", category: 0, opening_hours: "", merchant_picture: "", user: julia, average_rate: 4.0, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
-biocoop = Shop.create(name: "Biocoop", address: "7 Place de la Trinité, 31000 Toulouse", mobile: "0534424919", email: "", category: "Épicerie", opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.3)
+biocoop = Shop.create(name: "Biocoop", address: "7 Place de la Trinité, 31000 Toulouse", mobile: "0534424919", email: "", category: 0, opening_hours: "", merchant_picture: "", user: annelaure, average_rate: 4.3, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.")
 
 
 #Creation rentals
