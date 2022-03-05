ced3a6b7dcfa7293d37b16ec9f08c28a6f343abb
AnneLaure Le <annelaurele@hotmail.fr>
2022-03-05 11:42:32 +0100

    qrcode page

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -12,6 +12,8 @@ class RentalsController < ApplicationController
     @qr = RQRCode::QRCode.new("16")
     @shop = Shop.find(params[:shop_id])
     @rental = Rental.find(params[:rental_id])
+    # @menus = Menu.where(shop: @shop_id)
+    # @menu = Menu.find(params[:shop_id])
     @svg = @qr.as_svg(
     color: "000",
     shape_rendering: "crispEdges",
